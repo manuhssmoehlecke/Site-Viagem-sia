aac405046562c027f0b1255ec2264c9376219f7e
manuhssmoehlecke <manuelamoehlecke@gmail.com>
2024-03-12 17:04:14 -0300

    Ajuste do tamanho do comentário

--- resources/views/japao.blade.php
+++ resources/views/japao.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Mamíferos Marinhos</title>
+    <title>Japão</title>
     <link rel="stylesheet" href="css/japao.css" />
   </head>
 
